bdedb2cc338ae838d86633136390c159232a7e55
Lal'C Mellk Mal <lalc@doushio.com>
2017-05-13 09:46:55 -0400

    Fix right-side image expansion

--- client/options.js
+++ client/options.js
@@ -303,10 +303,7 @@ function toggle_expansion(img, event) {
 		return;
 	event.preventDefault();
 	var expand = !img.data('thumbSrc');
-	if (expand)
-		img.closest('figure').addClass('expanded');
-	else
-		img.closest('figure').removeClass('expanded');
+	img.closest('article').toggleClass('expanded', expand);
 	var $imgs = img;
 	if (THREAD && (event.altKey || event.shiftKey)) {
 		var post = img.closest('article');
