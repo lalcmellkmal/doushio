f7d86821b48d51ddc3f595d867e2e8cfd9fbff81
Lal'C Mellk Mal <lalc@doushio.com>
2020-10-10 14:29:10 -0700

    Modernize imager/jobs.js

--- imager/jobs.js
+++ imager/jobs.js
@@ -1,24 +1,27 @@
-var events = require('events'),
+const events = require('events'),
     util = require('util'),
     winston = require('winston');
 
-var JOB_LIMIT = 1;
-var JOB_TIMEOUT = 30 * 1000;
+// we only run one job at a time by default; thumbnailing is fast but can use a lot of RAM
+let JOB_LIMIT = 1;
+// to fix: sometimes the imager breaks down for a while and then works again
+// (some job must be stalling and getting timed out)
+let JOB_TIMEOUT = 30 * 1000;
 
-var JOB_QUEUE = [];
-var JOBS_RUNNING = 0;
+const JOB_QUEUE = [];
+let JOBS_RUNNING = 0;
 
 function schedule(job, cb) {
 	if (job && job.jobRunning)
-		winston.warn("Job "+job.describe_job()+" already running!");
-	else if (job && JOB_QUEUE.indexOf(job) >= 0)
-		winston.warn("Job "+job.describe_job()+" already scheduled!");
+		winston.warn(`Job ${job.describe_job()} already running!`);
+	else if (job && JOB_QUEUE.includes(job))
+		winston.warn(`Job ${job.describe_job()} already scheduled!`);
 	else if (job) {
 		JOB_QUEUE.push(job);
 		if (cb) {
 			/* Sucks */
 			job.once('finish', cb);
-			job.once('timeout', cb.bind(null, "Timed out."));
+			job.once('timeout', () => cb("Timed out."));
 		}
 	}
 
@@ -27,6 +30,7 @@ function schedule(job, cb) {
 }
 exports.schedule = schedule;
 
+/// Subclasses must implement `perform_job` and should override `describe_job`.
 function Job() {
 	events.EventEmitter.call(this);
 }
@@ -35,7 +39,7 @@ exports.Job = Job;
 
 Job.prototype.start_job = function () {
 	if (this.jobRunning) {
-		winston.warn(this.describe_job() + " already started!");
+		winston.warn(`${this.describe_job()} already started!`);
 		return;
 	}
 	JOBS_RUNNING++;
@@ -46,8 +50,7 @@ Job.prototype.start_job = function () {
 
 Job.prototype.finish_job = function (p1, p2) {
 	if (!this.jobRunning) {
-		winston.warn("Attempted to finish stopped job: "
-				+ this.describe_job());
+		winston.warn(`Attempted to finish stopped job: ${this.describe_job()}`);
 		return;
 	}
 	clearTimeout(this.jobTimeout);
@@ -55,26 +58,26 @@ Job.prototype.finish_job = function (p1, p2) {
 	this.jobRunning = false;
 	JOBS_RUNNING--;
 	if (JOBS_RUNNING < 0)
-		winston.warn("Negative job count: " + JOBS_RUNNING);
+		winston.warn(`Negative job count: ${JOBS_RUNNING}`);
 	/* use `arguments` later */
 	this.emit('finish', p1, p2);
 	schedule(null);
 };
 
 Job.prototype.timeout_job = function () {
-	var desc = this.describe_job();
+	const desc = this.describe_job();
 	if (!this.jobRunning) {
-		winston.warn("Job " + desc + " timed out though finished?!");
+		winston.warn(`Job ${desc} timed out though finished?!`);
 		return;
 	}
 
-	winston.error(desc + " timed out.");
+	winston.error(`${desc} timed out.`);
 
 	this.jobTimeout = 0;
 	this.jobRunning = false;
 	JOBS_RUNNING--;
 	if (JOBS_RUNNING < 0)
-		winston.warn("Negative job count: " + JOBS_RUNNING);
+		winston.warn(`Negative job count: ${JOBS_RUNNING}`);
 
 	this.emit('timeout');
 	schedule(null);
