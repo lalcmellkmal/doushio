6a93aefc3e69d46b89d9b59be71ff196b063520c
Lal'C Mellk Mal <lalc@doushio.com>
2021-03-15 15:37:51 -0700

    Async archive dumper

--- archive/dump.js
+++ archive/dump.js
@@ -1,92 +1,100 @@
-/* Dumps the given thread to JSON.
- * Not production-ready.
+/* Dumps the given thread as JSON and HTML to `www/archive/`
+ * Writes the IP addresses as JSON to `./authdump/<thread>.json`
  */
 
-var _ = require('../lib/underscore'),
-    async = require('async'),
-    caps = require('../server/caps'),
+const caps = require('../server/caps'),
     db = require('../db'),
     etc = require('../etc'),
-    fs = require('fs'),
-    joinPath = require('path').join,
-    render = require('../server/render'),
-    util = require('util');
+    { createWriteStream } = require('fs'),
+    { join } = require('path'),
+    { write_thread_head, write_thread_html, write_page_end } = require('../server/render');
 
-var DUMP_DIR = 'www/archive';
-var AUTH_DUMP_DIR = 'authdump';
+const DUMP_DIR = 'www/archive';
+const AUTH_DUMP_DIR = 'authdump';
 
-var DUMP_IDENT = {ip: '127.0.0.1', auth: 'dump'};
+const DUMP_IDENT = {ip: '127.0.0.1', auth: 'dump'};
 
-function Dumper(reader, out) {
-	this.out = out;
-	this.reader = reader;
-	_.bindAll(this);
-	reader.on('thread', this.on_thread);
-	reader.on('post', this.on_post);
-	reader.on('endthread', this.on_endthread);
+class JsonDumper {
+	constructor(reader, out) {
+		this.out = out;
+		this.reader = reader;
+		reader.on('thread', this.on_thread);
+		reader.on('post', this.on_post);
+		reader.on('endthread', this.on_endthread);
+	}
+
+	on_thread = (op_post) => {
+		if (this.needComma) {
+			this.out.write(',\n');
+			this.needComma = false;
+		}
+		this.op = op_post.num;
+		this.out.write('[\n' + JSON.stringify(tweak_post(op_post)));
+	}
+
+	on_post = (post) => {
+		this.out.write(',\n' + JSON.stringify(tweak_post(post, this.op)));
+	}
+
+	on_endthread = () => {
+		this.out.write('\n]');
+		this.needComma = true;
+		this.op = null;
+	}
+
+	destroy() {
+		this.reader.removeListener('thread', this.on_thread);
+		this.reader.removeListener('post', this.on_post);
+		this.reader.removeListener('endthread', this.on_endthread);
+		this.reader = null;
+		this.out = null;
+	}
 }
-var D = Dumper.prototype;
 
-D.on_thread = function (op_post) {
-	if (this.needComma) {
-		this.out.write(',\n');
+class AuthDumper {
+	constructor(reader, out) {
+		this.out = out;
+		this.reader = reader;
+		reader.on('thread', this.on_thread);
+		reader.on('post', this.on_post);
+		reader.on('endthread', this.on_endthread);
+	}
+
+	on_thread = post => {
+		this.out.write('{"ips":{');
+
+		if (post.num && post.ip) {
+			this.out.write(`"${post.num}":${JSON.stringify(post.ip)}`);
+			this.needComma = true;
+		}
+	}
+
+	on_post = post => {
+		if (post.num && post.ip) {
+			if (this.needComma)
+				this.out.write(',');
+			else
+				this.needComma = true;
+			this.out.write(`"${post.num}":${JSON.stringify(post.ip)}`);
+		}
+	}
+
+	on_endthread = () => {
+		this.out.write('}}');
 		this.needComma = false;
 	}
-	this.op = op_post.num;
-	this.out.write('[\n' + JSON.stringify(tweak_post(op_post)));
-};
 
-D.on_post = function (post) {
-	this.out.write(',\n' + JSON.stringify(tweak_post(post, this.op)));
-};
-
-D.on_endthread = function () {
-	this.out.write('\n]');
-	this.needComma = true;
-	this.op = null;
-};
-
-D.destroy = function () {
-	this.reader.removeListener('thread', this.on_thread);
-	this.reader.removeListener('post', this.on_post);
-	this.reader.removeListener('endthread', this.on_endthread);
-	this.reader = null;
-	this.out = null;
-};
-
-
-function AuthDumper(reader, out) {
-	Dumper.call(this, reader, out);
+	destroy() {
+		this.reader.removeListener('thread', this.on_thread);
+		this.reader.removeListener('post', this.on_post);
+		this.reader.removeListener('endthread', this.on_endthread);
+		this.reader = null;
+		this.out = null;
+	}
 }
-util.inherits(AuthDumper, Dumper);
-var AD = AuthDumper.prototype;
-
-AD.on_thread = function (post) {
-	this.out.write('{"ips":{');
-
-	if (post.num && post.ip) {
-		this.out.write('"'+post.num+'":'+JSON.stringify(post.ip));
-		this.needComma = true;
-	}
-};
-
-AD.on_post = function (post) {
-	if (post.num && post.ip) {
-		if (this.needComma)
-			this.out.write(',');
-		else
-			this.needComma = true;
-		this.out.write('"'+post.num+'":'+JSON.stringify(post.ip));
-	}
-};
-
-AD.on_endthread = function () {
-	this.out.write('}}');
-	this.needComma = false;
-};
 
 function tweak_post(post, known_op) {
-	post = _.clone(post);
+	post = { ...post };
 
 	/* thread-only */
 	if (typeof post.tags == 'string')
@@ -115,76 +123,67 @@ function tweak_post(post, known_op) {
 	return post;
 }
 
-function dump_thread(op, board, ident, outputs, cb) {
+function dump_thread(op, board, ident, outputs) {
 	if (!caps.can_access_board(ident, board))
-		return cb(404);
+		return Promise.reject(404);
 	if (!caps.can_access_thread(ident, op))
-		return cb(404);
+		return Promise.reject(404);
 
-	var yaku = new db.Yakusoku(board, ident);
-	var reader = new db.Reader(yaku);
-	reader.get_thread(board, op, {});
-	reader.once('nomatch', function () {
-		cb(404);
-		yaku.disconnect();
-	});
-	reader.once('redirect', function (op) {
-		cb('redirect', op);
-		yaku.disconnect();
-	});
-	reader.once('begin', function (preThread) {
-		var dumper = new Dumper(reader, outputs.json);
-		var authDumper = new AuthDumper(reader, outputs.auth);
+	const yaku = new db.Yakusoku(board, ident);
+	return new Promise((resolve, reject) => {
+		yaku.once('error', reject);
+		const reader = new db.Reader(yaku);
+		reader.get_thread(board, op, {});
+		reader.once('nomatch', () => reject(404));
+		reader.once('redirect', op => reject('redirect'));
+		reader.once('error', reject);
+		reader.once('begin', ({ subject }) => {
+			const { json, auth, html } = outputs;
+			const jsonDumper = new JsonDumper(reader, json);
+			const authDumper = new AuthDumper(reader, auth);
 
-		var out = outputs.html;
-		render.write_thread_head(out, '', board, op, {
-			subject: preThread.subject,
+			write_thread_head(html, '', board, op, { subject });
+
+			const fakeReq = {ident, headers: {}};
+			const opts = {fullPosts: true, board};
+			write_thread_html(reader, fakeReq, html, opts);
+
+			reader.once('end', () => {
+				json.write('\n');
+				auth.write('\n');
+				write_page_end(html, ident, true);
+				resolve();
+			});
 		});
-
-		var fakeReq = {ident: ident, headers: {}};
-		var opts = {fullPosts: true, board: board};
-		render.write_thread_html(reader, fakeReq, out, opts);
-
-		reader.once('end', function () {
-			outputs.json.write('\n');
-			outputs.auth.write('\n');
-			render.write_page_end(out, ident, true);
-			yaku.disconnect();
-			cb(null);
-		});
-	});
-
-	function on_err(err) {
-		yaku.disconnect();
-		cb(err);
-	}
-	reader.once('error', on_err);
-	yaku.once('error', on_err);
+	}).finally(() => yaku.disconnect());
 }
 
-function close_stream(stream, cb) {
-	if (!stream.writable)
-		return cb(null);
-	if (stream.write(''))
-		close();
-	else
-		stream.once('drain', close);
+function close_stream(stream) {
+	return new Promise((resolve, reject) => {
+		if (!stream.writable)
+			return resolve();
+		if (stream.write(''))
+			close();
+		else
+			stream.once('drain', close);
 
-	function close() {
-		// deal with process.stdout not being closable
-		try {
-			stream.destroySoon(function (err) {
-				if (cb)
-					cb(err);
-				cb = null;
-			});
+		function close() {
+			// deal with process.stdout not being closable
+			try {
+				stream.destroySoon(err => {
+					if (resolve) {
+						err ? reject(err) : resolve();
+					}
+					resolve = reject = null;
+				});
+			}
+			catch (e) {
+				if (resolve)
+					resolve();
+				resolve = reject = null;
+			}
 		}
-		catch (e) {
-			if (cb)
-				cb(null);
-			cb = null;
-		}
-	}
+	});
 }
 
 async function load_state() {
@@ -209,39 +208,31 @@ async function dump() {
 	if (!board)
 		board = db.first_tag_of(op);
 	if (!board) {
-		console.error(op + ' has no tags.');
+		console.error(`#${op} has no tags.`);
 		process.exit(-1);
 	}
 
-	console.log('Dumping thread...');
+	console.log(`Dumping thread #${op} on /${board}/...`);
 
-	const base = joinPath(DUMP_DIR, op.toString());
-	const authBase = joinPath(AUTH_DUMP_DIR, op.toString());
+	const base = join(DUMP_DIR, op.toString());
+	const authBase = join(AUTH_DUMP_DIR, op.toString());
 	const outputs = {
-		auth: fs.createWriteStream(authBase + '.json'),
-		json: fs.createWriteStream(base + '.json'),
-		html: fs.createWriteStream(base + '.html'),
+		auth: createWriteStream(authBase + '.json'),
+		json: createWriteStream(base + '.json'),
+		html: createWriteStream(base + '.html'),
 	};
 
-	dump_thread(op, board, DUMP_IDENT, outputs, function (err) {
-		if (err)
-			throw err;
+	await dump_thread(op, board, DUMP_IDENT, outputs);
 
-		const streams = [];
-		for (let k in outputs)
-			streams.push(outputs[k]);
-		async.each(streams, close_stream, quit);
-	});
+	for (let k in outputs)
+		await close_stream(outputs[k]);
 
-	function quit() {
-		// crappy flush for stdout (can't close it)
-		if (process.stdout.write(''))
-			process.exit(0);
-		else
-			process.stdout.on('drain', function () {
-				process.exit(0);
-			});
-	}
+	console.log(`Wrote HTML to: ${base}.html`);
+	// crappy flush for stdout (can't close it)
+	if (process.stdout.write(''))
+		process.exit(0);
+	else
+		process.stdout.on('drain', () => process.exit(0));
 }
 
 if (require.main === module)
